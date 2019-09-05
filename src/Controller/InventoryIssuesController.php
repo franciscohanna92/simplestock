410f5e920c014e3e515c600a9a21579aea0a0044
Francisco Hanna <franciscohanna92@gmail.com>
2019-09-05 00:15:33 -0300

    logica de controlador

--- src/Controller/InventoryIssuesController.php
+++ src/Controller/InventoryIssuesController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use App\Controller\AppController;
@@ -20,32 +21,33 @@ class InventoryIssuesController extends AppController
      */
     public function index()
     {
-$searchQuery = $this->request->getQuery('searchQuery');
-        $pageTitle = 'Listado inventoryIssues';
+        $searchQuery = $this->request->getQuery('searchQuery');
+        $pageTitle = 'Listado de salidas';
         $this->paginate = [
-            'contain' => ['Employees', 'BuildingSites', 'Companies']
+            'contain' => ['Companies']
         ];
         $inventoryIssues = $this->paginate($this->InventoryIssues);
 
         $this->set(compact('inventoryIssues', 'pageTitle', 'searchQuery'));
     }
 
-/**
-* View method
-*
-* @param string|null $id Inventory Issue id.
-* @return \Cake\Http\Response|void
-* @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-*/
-public function view($id = null)
-{
-$pageTitle = 'View inventoryIssue';
-$inventoryIssue = $this->InventoryIssues->get($id, [
-'contain' => ['Employees', 'BuildingSites', 'Companies', 'Articles']
-]);
+    /**
+     * View method
+     *
+     * @param string|null $id Inventory Receipt id.
+     * @return \Cake\Http\Response|void
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $pageTitle = 'Detalle de salida';
+        $inventoryIssue = $this->InventoryIssues->get($id, [
+            'contain' => ['Companies', 'Articles']
+        ]);
 
-$this->set(compact('inventoryIssue', 'pageTitle'));
-}
+
+        $this->set(compact('inventoryIssue', 'pageTitle'));
+    }
 
     /**
      * Add method
@@ -54,70 +56,81 @@ $this->set(compact('inventoryIssue', 'pageTitle'));
      */
     public function add()
     {
-$pageTitle = 'Add inventoryIssue';
+        $pageTitle = 'Registrar nueva salida';
         $inventoryIssue = $this->InventoryIssues->newEntity();
         if ($this->request->is('post')) {
             $inventoryIssue = $this->InventoryIssues->patchEntity($inventoryIssue, $this->request->getData());
             $inventoryIssue['created_by'] = $this->Auth->user()['id'];
             $inventoryIssue['company_id'] = $this->Auth->user()['company_id'];
+
+            foreach ($inventoryIssue['articles'] as $article) {
+                $article['stock'] -= $article->_joinData['quantity'];
+            }
+
             if ($this->InventoryIssues->save($inventoryIssue)) {
-                $this->Flash->success(__('The inventory issue has been saved.'));
+                $this->Flash->success(__('The inventory receipt has been saved.'));
+
 
                 return $this->redirect(['action' => 'index']);
             }
-            $this->Flash->error(__('The inventory issue could not be saved. Please, try again.'));
+            $this->Flash->error(__('The inventory receipt could not be saved. Please, try again.'));
         }
-        $employees = $this->InventoryIssues->Employees->find('list', ['limit' => 200]);
-        $buildingSites = $this->InventoryIssues->BuildingSites->find('list', ['limit' => 200]);
-        $companies = $this->InventoryIssues->Companies->find('list', ['limit' => 200]);
-        $articles = $this->InventoryIssues->Articles->find('list', ['limit' => 200]);
-        $this->set(compact('inventoryIssue', 'employees', 'buildingSites', 'companies', 'articles', 'pageTitle'));
+        $articles = $this->InventoryIssues->Articles->find('all')->contain(['Units'])->toArray();
+        $this->set(compact('inventoryIssue', 'companies', 'articles', 'pageTitle'));
     }
 
     /**
      * Edit method
      *
-     * @param string|null $id Inventory Issue id.
+     * @param string|null $id Inventory Receipt id.
      * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
     public function edit($id = null)
     {
-$pageTitle = 'Edit inventoryIssue';
+        $pageTitle = 'Edit inventoryIssue';
         $inventoryIssue = $this->InventoryIssues->get($id, [
             'contain' => ['Articles']
         ]);
         if ($this->request->is(['patch', 'post', 'put'])) {
             $inventoryIssue = $this->InventoryIssues->patchEntity($inventoryIssue, $this->request->getData());
             if ($this->InventoryIssues->save($inventoryIssue)) {
-                $this->Flash->success(__('The inventory issue has been saved.'));
+                $this->Flash->success(__('The inventory receipt has been saved.'));
 
                 return $this->redirect(['action' => 'index']);
             }
-            $this->Flash->error(__('The inventory issue could not be saved. Please, try again.'));
+            $this->Flash->error(__('The inventory receipt could not be saved. Please, try again.'));
         }
-        $employees = $this->InventoryIssues->Employees->find('list', ['limit' => 200]);
-        $buildingSites = $this->InventoryIssues->BuildingSites->find('list', ['limit' => 200]);
-        $companies = $this->InventoryIssues->Companies->find('list', ['limit' => 200]);
         $articles = $this->InventoryIssues->Articles->find('list', ['limit' => 200]);
-        $this->set(compact('inventoryIssue', 'employees', 'buildingSites', 'companies', 'articles', 'pageTitle'));
+        $this->set(compact('inventoryIssue', 'companies', 'articles', 'pageTitle'));
     }
 
     /**
      * Delete method
      *
-     * @param string|null $id Inventory Issue id.
+     * @param string|null $id Inventory Receipt id.
      * @return \Cake\Http\Response|null Redirects to index.
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
     public function delete($id = null)
     {
         $this->request->allowMethod(['post', 'delete']);
-        $inventoryIssue = $this->InventoryIssues->get($id);
+        $inventoryIssue = $this->InventoryIssues->get($id, [
+            'contain' => [
+                'Articles'
+            ]
+        ]);
+
+        foreach ($inventoryIssue->articles as $article) {
+            $existingArticle = $this->InventoryIssues->Articles->get($article['id']);
+            $existingArticle->stock = $existingArticle->stock + $article['_joinData']['quantity'];
+            $this->InventoryIssues->Articles->save($existingArticle);
+        }
+
         if ($this->InventoryIssues->delete($inventoryIssue)) {
-            $this->Flash->success(__('The inventory issue has been deleted.'));
+            $this->Flash->success(__('The inventory receipt has been deleted.'));
         } else {
-            $this->Flash->error(__('The inventory issue could not be deleted. Please, try again.'));
+            $this->Flash->error(__('The inventory receipt could not be deleted. Please, try again.'));
         }
 
         return $this->redirect(['action' => 'index']);
